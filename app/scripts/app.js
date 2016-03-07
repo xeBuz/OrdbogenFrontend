ca405acb77b957d832f9e0103112f58168749728
Jesus Roldan <xebuz18@gmail.com>
2016-03-06 22:10:50 -0300

    Pagination added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,7 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
+    'LocalStorageModule',
     'API',
     'countries',
   ])
--- app/scripts/modules/countries/controllers/countriesController.js
+++ app/scripts/modules/countries/controllers/countriesController.js
@@ -5,21 +5,19 @@
  * @description
  */
 angular.module('countries')
-    .controller('countriesController', ['$scope', 'APIService', function($scope, API) {
+    .controller('countriesController', ['$scope', 'APIService', 'localStorageService', function($scope, API, localStorageService) {
 
         $scope.countriesList = [];
-        $scope.currentPage = {
-            number: 1
-        };
+        $scope.currentPage = 1
 
         var getCountries = function(page) {
 
             API.get('countries',{},{page: page, count: 10}).then(function(data) {
-                console.log(data);
-                console.log(data.data);
                 if (data.status.code === 200) {
                     $scope.countriesList = data.data;
                     $scope.totalItems = data.length;
+                    $scope.next = !angular.isUndefined(data.links.next);
+                    $scope.prev = !angular.isUndefined(data.links.prev);
                 } else {
                     console.log("Error getting Countries")
                 }
@@ -37,9 +35,19 @@ angular.module('countries')
         };
 
         $scope.pageChanged = function() {
-            getCountries($scope.currentPage.number);
+            getCountries($scope.currentPage);
         };
 
-        getCountries($scope.currentPage.number);
+        $scope.nextPage = function() {
+            $scope.currentPage++;
+            getCountries($scope.currentPage);
+        }
+
+        $scope.prevPage = function() {
+            $scope.currentPage--;
+            getCountries($scope.currentPage);
+        }
+
+        getCountries($scope.currentPage);
 
 }]);
